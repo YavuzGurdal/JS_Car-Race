02ac98ff1ae96e11ffe97d391580a6eb39a5a376
YavuzGurdal <57366318+YavuzGurdal@users.noreply.github.com>
2020-01-21 19:27:20 +0100

    fonksiyonda degisiklik yapildi

--- main.js
+++ main.js
@@ -1,46 +1,38 @@
-
 function move() {  
 
-    var x = Math.floor(Math.random() * 5) + 1;
+    var carr1 = document.getElementById("car1");
+    var carr2 = document.getElementById("car2");
 
-    var y = Math.floor(Math.random() * 5) + 1;
-    
-    var carr1 = document.getElementById("car1");   
-    var car1Margin = x; 
-
-    var carr2 = document.getElementById("car2");   
-    var car2Margin = y;  
+    var position1 = 0;
+    var position2 = 0; 
 
     var sonuc = document.getElementById("p1")
 
-    var sayac = 0;
-
-    var idVar = setInterval(change, 40);  
+    var idVar = setInterval(change, 10);  
     
     
 
-    function change() {    
+    function change() { 
+        
+        position1 += Math.floor(Math.random() * 5) + 1;
+        position2 += Math.floor(Math.random() * 5) + 1;
+
+        carr1.style.left = `${10 + position1}px`;
+        carr2.style.left = 10 + position2 + "px";
+        // ikisi de aynı işi yapiyor
       
-        if (sayac == 80){
-            if ( x > y) {
+        if ( position1 >= 1000){
+                clearInterval(idVar);
                 sonuc.style.color = "red";
                 sonuc.innerHTML = "Kırmızı araba kazandı";
-            } else if ( x < y) {
-                sonuc.innerHTML = "Sarı araba kazandı";
-                sonuc.style.color = "#bdac13";
-            } else {
-                sonuc.innerHTML = "Berabere";
-            }
-
+        } else if ( position2 >= 1000) {
+            clearInterval(idVar);
+            sonuc.innerHTML = "Sarı araba kazandı";
+            sonuc.style.color = "#bdac13";
+        } else if (position1 >= 1000 && position2 >= 1000 && position1 == position2) {
             clearInterval(idVar)
-        } else {      
-            car1Margin++;        
-            carr1.style.marginLeft = car1Margin + "1px";
-
-            car2Margin++;        
-            carr2.style.marginLeft = car2Margin + "1px";
-  
-            sayac = sayac + 1;
-        }  
-    }
+            sonuc.innerHTML = "Berabere";
+        }
+        
+    } 
 }
