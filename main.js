1f31c0bbc3a464ddb594cf562f0adf290c1f784e
YavuzGurdal <57366318+YavuzGurdal@users.noreply.github.com>
2021-12-16 19:51:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-function move() {  
+function move() {
 
     var carr1 = document.getElementById("car1");
     var carr2 = document.getElementById("car2");
 
     var position1 = 0;
-    var position2 = 0; 
+    var position2 = 0;
 
     var sonuc = document.getElementById("p1")
 
-    var idVar = setInterval(change, 10);  
-    
-    
+    var idVar = setInterval(change, 10);
 
-    function change() { 
-        
+
+
+    function change() {
+
         position1 += Math.floor(Math.random() * 5) + 1;
         position2 += Math.floor(Math.random() * 5) + 1;
 
         carr1.style.left = `${10 + position1}px`;
         carr2.style.left = 10 + position2 + "px";
         // ikisi de aynı işi yapiyor
-      
-        if ( position1 >= 1000){
-                clearInterval(idVar);
-                sonuc.style.color = "red";
-                sonuc.innerHTML = "Red Car Won";
-        } else if ( position2 >= 1000) {
+
+        if (position1 >= 800) {
+            clearInterval(idVar);
+            sonuc.style.color = "red";
+            sonuc.innerHTML = "Red Car Won";
+        } else if (position2 >= 800) {
             clearInterval(idVar);
             sonuc.innerHTML = "Yellow Car Won";
             sonuc.style.color = "#bdac13";
@@ -33,6 +33,6 @@ function move() {
             clearInterval(idVar)
             sonuc.innerHTML = "Berabere";
         }
-        
-    } 
+
+    }
 }
